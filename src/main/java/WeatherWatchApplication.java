1c5a468e2defd50233cfc7ce03a91329be9323de
Bren <bpgit503@gmail.com>
2025-02-23 15:31:36 +0100

    Created loop statement to loop through date the populate database with weather information

--- src/main/java/WeatherWatchApplication.java
+++ src/main/java/WeatherWatchApplication.java
@@ -1,13 +1,26 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class WeatherWatchApplication {
     public static void main(String[] args) throws Exception {
-        final String forecastURL = "http://api.weatherapi.com/v1/history.json?key=610ec3279f83484a9b2143452251302&q=Katowice&dt=2025-02-12";
 
         WeatherDataParser parser = new WeatherDataParser();
         try {
+            LocalDate startDate = LocalDate.of(2024, 2, 23);
+            LocalDate endDate = LocalDate.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
-            String JsonResponseWeatherData = parser.fetchWeatherData(forecastURL);
+            for (LocalDate date = startDate; !date.isAfter(endDate); date = date.plusDays(1)) {
+                String formattedDate = date.format(formatter);
+                String forecastURL = "http://api.weatherapi.com/v1/history.json?key=610ec3279f83484a9b2143452251302&q=Katowice&dt=" + formattedDate;
 
-            parser.parseAndStoreWeatherData(JsonResponseWeatherData);
+                // Fetch API data
+                String JsonResponseWeatherData = parser.fetchWeatherData(forecastURL);
+
+                // Parse and insert into DB
+                parser.parseAndStoreWeatherData(JsonResponseWeatherData);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
         }
